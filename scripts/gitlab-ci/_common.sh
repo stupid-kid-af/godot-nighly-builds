0c37a2abec0e436bfa3a22b7e8d35798f523935b
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-06-07 15:16:13 +0530

    Update _common.sh

--- scripts/gitlab-ci/_common.sh
+++ scripts/gitlab-ci/_common.sh
@@ -6,8 +6,8 @@
 # This script is always sourced before any other script
 
 # The directory where build artifacts are stored
-export ARTIFACTS_DIR
-ARTIFACTS_DIR="/artifacts"
+sudo export ARTIFACTS_DIR
+sudo ARTIFACTS_DIR="artifacts"
 
 # The directory the Godot Git repository will be cloned into
 export GODOT_DIR
