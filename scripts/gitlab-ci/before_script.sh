496fb7081510532b204f709e3146eaaebf418f82
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-06-07 15:19:35 +0530

    Update before_script.sh

--- scripts/gitlab-ci/before_script.sh
+++ scripts/gitlab-ci/before_script.sh
@@ -24,11 +24,11 @@ else
   # Ubuntu
   sudo apt-get update -qq
   sudo apt-get install -qqq software-properties-common
-  add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  apt-get update -qq
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+  sudo apt-get update -qq
 
   # SCons will be installed using pip so it can use Python 3.
-  apt-get install -qqq git cmake zip unzip build-essential pkg-config \
+  sudo apt-get install -qqq git cmake zip unzip build-essential pkg-config \
       libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libcairo2 \
       libglu-dev libasound2-dev libpulse-dev libfreetype6-dev \
       libssl-dev libudev-dev libxrandr-dev libxi-dev curl \
