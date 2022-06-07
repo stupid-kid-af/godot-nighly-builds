dc72cff9c46d457445ac07584ff234d231aa8ff8
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-06-07 17:58:59 +0530

    Update linux_editor.sh

--- scripts/gitlab-ci/build/linux_editor.sh
+++ scripts/gitlab-ci/build/linux_editor.sh
@@ -25,6 +25,7 @@ scons platform=linuxbsd tools=yes target=debug \
 # Create Linux editor ZIP archive.
 cd bin/
 mv "godot.linuxbsd.tools.64" "godot"
+cd ..
 zip -r9 "editor/godot-linux-nightly-x86_64.zip" "godot"
 
 make_manifest "$ARTIFACTS_DIR/editor/godot-linux-nightly-x86_64.zip"
